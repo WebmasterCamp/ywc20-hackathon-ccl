bc746fcc6f89584a6d64167781d8c1d97cd353dd
suphanatchanlek <suphanatchanlek@gmail.com>
2025-06-02 01:06:53 +0700

    Update Navbabr

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar/>
     </div>
   );
 }
